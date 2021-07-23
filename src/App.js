848b69544075cbc534d2d5596d2b85fe992b2381
David Hay <david.j.hay@gmail.com>
2021-07-23 15:13:17 +0100

    post lesson 152

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
-import './App.css';
-
-function App() {
+const App = (props) => {
+  console.log("App function running");
+  const [showParagraph, setShowParagraph] = useState(false);
+  const toggleParagraphHandler = (event) => {
+    event.preventDefault();
+    setShowParagraph((prevState) => !prevState);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>this is new</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
-}
+};
 
 export default App;

e260c1925739edb23d019227b57ea85d701fdc1d
David Hay <david.j.hay@gmail.com>
2021-07-23 15:59:50 +0100

    post lesson 156

--- src/App.js
+++ src/App.js
@@ -6,16 +6,31 @@ import DemoOutput from "./components/Demo/DemoOutput";
 const App = (props) => {
   console.log("App function running");
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParagraphHandler = useCallback((event) => {
+  const toggleParagraphHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      if (allowToggle) {
+        console.log("toggle is allowed");
+        setShowParagraph((prevState) => !prevState);
+      } else {
+        console.log("toggle is not allowed");
+      }
+    },
+    [allowToggle]
+  );
+
+  const allowToggleHandler = (event) => {
     event.preventDefault();
-    setShowParagraph((prevState) => !prevState);
-  }, []);
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );

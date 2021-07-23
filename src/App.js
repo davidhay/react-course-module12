2328d53ac5aa2d2ada7cb1035c76a1336ba13d1d
David Hay <david.j.hay@gmail.com>
2021-07-23 15:22:36 +0100

    post lesson 153

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 const App = (props) => {
   console.log("App function running");
@@ -12,7 +13,7 @@ const App = (props) => {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>this is new</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );

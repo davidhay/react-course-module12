f8222e88feb8e356d1546856cd850f7b52c1f640
David Hay <david.j.hay@gmail.com>
2021-07-23 15:38:09 +0100

    post lesson 154

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const DemoOutput = (props) => {
+  console.log("DemoOutput RUNNING!");
   return <p>{props.show ? "This is new!" : ""}</p>;
 };
-export default DemoOutput;
+export default React.memo(DemoOutput);
